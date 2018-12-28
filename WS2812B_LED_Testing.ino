0cb8d2ba1184f0d942895f7d1343e63df2296f95
Aleksey-Solovyev <Aleksey-Solovyev@users.noreply.github.com>
2018-12-29 02:41:54 +0300

    Update

--- WS2812B_LED_Testing.ino
+++ WS2812B_LED_Testing.ino
@@ -11,11 +11,53 @@ void setup() {
   
   Serial.begin(9600);
   Serial.println("WS2812B Testing");
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  randomSeed(analogRead(0));
+  
   strip.begin();
    
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  int redColor;
+  int greenColor;
+  int blueColor;
+  int randDelay;
 
+  
+  randDelay = random(1000, 10000);
+  Serial.println("Delay is:");
+  Serial.println(randDelay);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("LED ON");
+  
+  redColor = random (0, 255);
+  Serial.println("Red Color: ");
+  Serial.println(redColor);
+  
+  greenColor = random (0, 255);
+  Serial.println("Green Color: ");
+  Serial.println(greenColor);
+  
+  blueColor = random (0, 255);
+  Serial.println("Blue Color: ");
+  Serial.println(blueColor);
+  
+  for (int i = 0; i < LED_COUNT; i++)
+  {
+    strip.setPixelColor(i, strip.Color(redColor, greenColor, blueColor));
+  }
+
+  strip.show();
+  
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("LED OFF");
+  
+  delay(randDelay);
+  
 }
